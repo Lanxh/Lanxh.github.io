a4d951df3c64d90c24b289c41759f87b0a04f9e5
Lanxh <leonardo.radicioni@gmail.com>
2019-11-10 19:28:27 +0100

    Initial commit

---
+++ js/scrollify-settings.js
@@ -0,0 +1,41 @@
+$(function(){
+  $.scrollify({
+    section: ".slide",
+		interstitialSection : ".footer",
+    easeing: "easeOutExpo",
+    scrollSpeed: 1100,
+		offset: 0,
+    scrollbars: true,
+    standardScrollElements: "",
+    setHeights: true,
+    overflowScroll: false,
+    updateHash: true,
+    touchScroll: false,
+    before:function() {},
+    after:function() {},
+    afterResize:function() {},
+    afterRender:function() {}
+  });
+});
+
+/** Section links **/
+
+$("#start-nav").click(function(){
+	$.scrollify.move(0);
+});
+
+$("#gamedev").click(function(){
+	$.scrollify.move(1);
+});
+
+$("#webdev").click(function(){
+	$.scrollify.move(2);
+});
+
+/*$("#archeo").click(function(){
+	$.scrollify.move(3);
+});
+
+$("#archeo-nav").click(function(){
+	$.scrollify.move(3);
+});*/
