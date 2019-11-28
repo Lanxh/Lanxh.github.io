ba131936981d5221804474ab8b6528b8a45af5b9
Lanxh <leonardo.radicioni@gmail.com>
2019-11-28 23:45:41 +0100

    Created JS function to set correctly gallery elements

--- js/custom-javascript.js
+++ js/custom-javascript.js
@@ -2,6 +2,13 @@ $(function () {
   var selectedClass = "";
   $(".filter").click(function () {
     selectedClass = $(this).attr("data-rel");
+		if (selectedClass == "all") {
+			$("#gallery").removeClass('d-flex justify-content-center').addClass('gallery');
+			$('.1, .2, .3, .4').removeClass('w-50');
+		} else {
+			$("#gallery").removeClass('gallery').addClass('d-flex justify-content-center');
+			$('.1, .2, .3, .4').addClass('w-50');
+		}
     $("#gallery").fadeTo(100, 0.1);
     $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
     setTimeout(function () {
