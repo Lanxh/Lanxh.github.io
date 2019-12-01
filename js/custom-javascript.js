b07a85e66e505002fb1262a2da310fb552a948f5
Lanxh <leonardo.radicioni@gmail.com>
2019-12-02 00:09:43 +0100

    Completed My Works section and in general all site

--- js/custom-javascript.js
+++ js/custom-javascript.js
@@ -3,11 +3,11 @@ $(function () {
   $(".filter").click(function () {
     selectedClass = $(this).attr("data-rel");
 		if (selectedClass == "all") {
-			$("#gallery").removeClass('d-flex justify-content-center').addClass('gallery');
-			$('.1, .2, .3, .4').removeClass('w-50');
+			$("#gallery").removeClass('d-flex justify-content-center align-items-center').addClass('gallery');
+			$('.1, .2, .3, .4').removeClass('w-50 mx-2');
 		} else {
-			$("#gallery").removeClass('gallery').addClass('d-flex justify-content-center');
-			$('.1, .2, .3, .4').addClass('w-50');
+			$("#gallery").removeClass('gallery').addClass('d-flex justify-content-center align-items-center');
+			$('.1, .2, .3, .4').addClass('w-50 mx-2');
 		}
     $("#gallery").fadeTo(100, 0.1);
     $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
