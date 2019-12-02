4eff988ab96cb2c8c11aa67a60a86f79f0960447
Lanxh <leonardo.radicioni@gmail.com>
2019-12-02 21:51:21 +0100

    Created js function to manage scrollify slide based on screen height

--- js/custom-javascript.js
+++ js/custom-javascript.js
@@ -5,9 +5,16 @@ $(function () {
 		if (selectedClass == "all") {
 			$("#gallery").removeClass('d-flex justify-content-center align-items-center').addClass('gallery');
 			$('.1, .2, .3, .4').removeClass('w-50 mx-2');
+			var slideHeight = $("#slide3").height();
+			if (slideHeight >= 1000) {
+				$("#myworks").removeClass('align-items-end').addClass('align-items-center');
+			} else {
+				$("#myworks").removeClass('align-items-center').addClass('align-items-end');
+			}
 		} else {
 			$("#gallery").removeClass('gallery').addClass('d-flex justify-content-center align-items-center');
 			$('.1, .2, .3, .4').addClass('w-50 mx-2');
+			$("#myworks").removeClass('align-items-end').addClass('align-items-center');
 		}
     $("#gallery").fadeTo(100, 0.1);
     $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
@@ -16,4 +23,13 @@ $(function () {
       $("#gallery").fadeTo(300, 1);
     }, 300);
   });
+});
+
+$(document).ready(function(){
+	var slideHeight = $("#slide3").height();
+	if (slideHeight >= 1000) {
+		$("#myworks").removeClass('align-items-end').addClass('align-items-center');
+	} else {
+		$("#myworks").removeClass('align-items-center').addClass('align-items-end');
+	}
 });
